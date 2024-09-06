beb6759973bf16d1e58d9c47a066aeb6bda252be
EliannyBello <elibellomanzo@gmail.com>
2024-09-06 16:17:55 +0000

    finalizado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	const [task, setTask] = useState("");
+	const [tasks, setTasks] = useState([]);
+
+	const addTask = (e) => {
+		e.preventDefault();
+		if (task === "") return;
+		setTasks([...tasks, task]);
+		setTask("");
+	};
+
+	const deleteTask = (i) => {
+		const updatedTasks = tasks.filter(task => task !== i);
+		setTasks(updatedTasks);
+	};
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1>To-Do List</h1>
+			<form onSubmit={addTask}>
+				<input
+					type="text"
+					value={task}
+					onChange={(e) => setTask(e.target.value)}
+					onKeyUp={(e) => setTask(e.target.value)}
+					placeholder="Escribe una tarea..."
+				/>
+			</form>
+			<ul>
+				{tasks.length === 0 ? (
+					<li>No hay tareas pendientes</li>
+				) : (
+					tasks.map((task, index) => (
+						<li key={index}>
+							{task}{" "}
+							<button onClick={() => deleteTask(task)}>X</button>
+						</li>
+					))
+				)}
+			</ul>
 		</div>
 	);
 };
